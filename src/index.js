71c26ffaaf6e1483971224d6c8c34ed76f0e3ad5
Rafael <rafadellaquila@gmail.com>
2019-11-24 20:55:11 -0300

    :sparkles: create project with create-react-app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/*
+import "./styles/settings/spaces.css";
+import "./styles/settings/colors.css";
+import "./styles/settings/sizes.css";
+import "./styles/generic/reset.css";
+import "./styles/elements/base.css";
+*/
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
