81848f586cc6694aea7a80b3a9f3a9680dfefde4
Rafael <rafadellaquila@gmail.com>
2019-11-25 14:58:31 -0300

    :sparkles: create Header component w/ HeaderName inside

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import avatar from './img/avatar.png';
+// import avatar from './img/avatar.png';
 import './App.css';
+import HeaderPage from './component/HeaderPage';
 
 function App() {
-  return (
-    <div className="under-construction">
-      <header className="content">
-        <img src={avatar} className="avatar" alt="logo" />
-        <p className="text">Em construção.</p>
-        <a
-          className="link"
-          href="https://github.com/rafaeldellaquila/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-          className="link"
-          href="mailto:rafadellaquila@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          rafadellaquila@gmail.com
-        </a>
-      </header>
-    </div>
-  );
+  return <HeaderPage />;
 }
 
 export default App;
