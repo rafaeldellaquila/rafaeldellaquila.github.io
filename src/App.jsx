6db058d3fbcff12eb793b0a49cfbdb736090f3ea
Rafael <rafadellaquila@gmail.com>
2019-11-25 21:14:57 -0300

    :sparkles: create BannerTitle object for BannerHome component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 // import avatar from './img/avatar.png';
 import './App.css';
 import HeaderPage from './component/HeaderPage';
+import BannerTitle from './objects/BannerTitle';
 
 function App() {
-  return <HeaderPage />;
+  return (
+    <>
+      <HeaderPage />
+      <BannerTitle />
+    </>
+  );
 }
 
 export default App;
