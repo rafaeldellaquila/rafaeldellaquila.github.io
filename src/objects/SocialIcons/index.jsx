a69bff81831e26341803c3b8084576c3ba1b04c1
Rafael <rafadellaquila@gmail.com>
2019-11-26 23:08:06 -0300

    :art: improve semantic

--- src/objects/SocialIcons/index.jsx
+++ src/objects/SocialIcons/index.jsx
@@ -8,49 +8,51 @@ import Linkedin from '../../img/icon-linkedin.png';
 function SocialIcons() {
   return (
     <dl className="social-icons">
-      <dd className="social">
+      <ul className="social">
         <a
           href="https://github.com/rafaeldellaquila"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img className="social-icons" src={Github} alt="Icone do Github" />
+          <img className="icon" src={Github} alt="Icone do Github" />
         </a>
+      </ul>
+      <ul className="social">
         <a
           href="https://www.linkedin.com/in/rafaeldellaquila/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img
-            className="social-icons"
-            src={Linkedin}
-            alt="Icone do Linkedin"
-          />
+          <img className="icon" src={Linkedin} alt="Icone do Linkedin" />
         </a>
-
+      </ul>
+      <ul className="social">
         <a
           href="https://www.behance.net/rafaeldellaquila"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img className="social-icons" src={Behance} alt="Icone do behance" />
+          <img className="icon" src={Behance} alt="Icone do behance" />
         </a>
+      </ul>
+      <ul className="social">
         <a
           href="mailto:rafadellaquila@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img className="social-icons" src={Email} alt="Icone do E-mail" />
+          <img className="icon" src={Email} alt="Icone do E-mail" />
         </a>
-
+      </ul>
+      <ul className="social">
         <a
           href="https://t.me/rafadellaquila"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img className="social-icons" src={Telegram} alt="Icone do E-mail" />
+          <img className="icon" src={Telegram} alt="Icone do E-mail" />
         </a>
-      </dd>
+      </ul>
     </dl>
   );
 }
