f430fffc5d111b4be8d4e5e6935388f6f32c395f
Rafael <rafadellaquila@gmail.com>
2019-11-27 23:45:00 -0300

    :sparkles: add on app

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderPage from './component/HeaderPage';
 import BannerHome from './component/BannerHome';
-import SocialIcons from './objects/SocialIcons';
-import AboutMe from './objects/AboutMe';
+import AboutSection from './component/AboutSection';
 
 function App() {
   return (
     <>
       <HeaderPage />
       <BannerHome />
-      <SocialIcons />
-      <AboutMe />
+      <AboutSection />
     </>
   );
 }
