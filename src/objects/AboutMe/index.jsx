4c88d5111f2fbb335af8a271c84cc941cc413ebf
Rafael <rafadellaquila@gmail.com>
2019-11-27 23:03:36 -0300

    :lipstick: create style object about-me

--- src/objects/AboutMe/index.jsx
+++ src/objects/AboutMe/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import './styles.css';
 
 function AboutMe() {
   return (
-    <article>
-      <h1>
+    <article className="about-me">
+      <h1 className="title">
         Me chamo Rafael Dell’Aquila, sou Designer e aspirante a Front-end.
       </h1>
-      <p>
+      <p className="description">
         Comecei a trabalhar em agências aos 18 anos e durante esse tempo de
         experiência pude trabalhar com diversas marcas atuando como Diretor de
         arte e Designer e agora estou em busca de um novo desafio, o Front-end.
       </p>
-      <a href="#to-do">
+      <a href="#to-do" className="portifolio">
         Aqui você poderá ver alguns projetos que eu estou fazendo.
       </a>
     </article>
